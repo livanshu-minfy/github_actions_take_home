5a458b811d181218ec9ba3e486f8b2321d366dbe
livanshu-minfy <livanshu.saini@minfytech.com>
2025-06-21 15:40:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
   res.send(`Hello from our container! Version: ${version}. Deployed automatically! -- livanshu`);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on <http://localhost>:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
